99e6c89e5ff77418748f9d3ca85db51e79b25a0c
Kleber Vales <klebervales.dev@gmail.com>
2025-08-14 10:22:21 -0300

    Setup Spring Boot with Java 21 and JUnit 5

---
+++ src/main/java/com/example/springmockmvcdemo/SpringMockmvcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmockmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMockmvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMockmvcDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springmockmvcdemo/SpringMockmvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmockmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockmvcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
